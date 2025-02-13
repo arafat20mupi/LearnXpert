2b8296ca33784c1e530c8f07417efc915a8a6504
ushanasif <ushanasif@gmail.com>
2025-02-13 19:09:56 +0600

    payment detail update after payment successful

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+
+import { useEffect } from "react";
 import SUCCESSIMAGE from "../../assets/success.gif"
+import { useLocation } from "react-router-dom";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+
 
 const Success = () => {
+  const axios = useAxiosPublic();
+  const location = useLocation();
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const sessionId = new URLSearchParams(location.search).get('session_id');
+
+      if (sessionId) {
+        const response = await axios(`http://localhost:5000/api/retrieve/${sessionId}`);
+        console.log(response.data);
+      }
+    };
+
+    fetchSession();
+  }, [location]);
+
   return (
     <div className='bg-slate-200 w-full max-w-md mx-auto flex justify-center items-center flex-col p-4 m-2 rounded'>
       <img
