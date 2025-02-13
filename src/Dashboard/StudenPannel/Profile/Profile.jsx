7ef7080b5a77930c5f4159c77860bd409a13f8e6
unknown <arafat.innotimes.net@gmail.com>
2025-02-13 21:19:53 +0600

    Payment section all ok

--- src/Dashboard/StudenPannel/Profile/Profile.jsx
+++ src/Dashboard/StudenPannel/Profile/Profile.jsx
@@ -1,106 +1,103 @@
-import {loadStripe} from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 
 
 const StudentProfile = () => {
-   const {user} = useContext(AuthContext);
-   const [userDetail, setUserDetail] = useState({});
-   const [paymentDetail, setPaymentDetail] = useState({});
-   const axios = useAxiosPublic();
-   
+  const { user } = useContext(AuthContext);
+  const [userDetail, setUserDetail] = useState({});
+  const [paymentDetail, setPaymentDetail] = useState({});
+  const axios = useAxiosPublic();
 
 
-   const getCurrentUser = async() => {
+
+  const getCurrentUser = async () => {
     try {
       const response = await axios(`/api/get-single-student/${user?.uid}`);
       const data = await response?.data;
       setUserDetail(data);
-      console.log(data);
-      } catch (error) {
-          console.log(error.message);
-      }
-   };
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-   const getPaymentDetail = async() => {
-      try {
-        const response = await axios(`/api/payment-details/${user?.uid}`);
-        const data = await response?.data;
-        setPaymentDetail(data);
-        console.log(data);
-      } catch (error) {
-         console.log(error.message);
-      }
-   }
+  const getPaymentDetail = async () => {
+    try {
+      const response = await axios(`/api/payment-details/${user?.uid}`);
+      const data = await response?.data;
+      setPaymentDetail(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-    useEffect(()=>{
-        getCurrentUser();
-        getPaymentDetail();
-    }, [axios, user]);
+  useEffect(() => {
+    getCurrentUser();
+    getPaymentDetail();
+  }, [axios, user]);
 
 
-    const photo=
+  const photo =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYWBQmFSOxkbxNfQV0oQXHQ95vVcC-jM4luw&s"
 
 
 
-  const handlePayment = async(month, year, duePayment) => {
+  const handlePayment = async (month, year, duePayment) => {
     const stripe = await loadStripe("pk_test_51QPkuRGLRxtB32IDebIxkMEvw5raa70RYN0qrXmk5R3gcXVJtl2o3PZoYFwvXrsvQQod1HftIp4TvyfIMs9wq4qD00BoDzot7v");
-    const info = {name: paymentDetail?.name, firebaseUid:paymentDetail?.firebaseUid, email: paymentDetail?.email, month, year, duePayment}
+    const info = { name: paymentDetail?.name, firebaseUid: paymentDetail?.firebaseUid, email: paymentDetail?.email, month, year, duePayment }
 
-    const response = await axios.post('http://localhost:5000/api/checkout', {info});
+    const response = await axios.post('http://localhost:5000/api/checkout', { info });
     const data = await response.data;
-  
 
-    const result = stripe.redirectToCheckout({
-        sessionId: data.id
+    stripe.redirectToCheckout({
+      sessionId: data.id
     });
-    
+
   }
 
   return (
     <div className="bg-gray-100 py-10">
       <div className="flex justify-center items-center">
         <div className="space-y-8">
-            <div
-              className="max-w-md p-6 mx-auto bg-white shadow-lg rounded-lg"
-            >
-              <div className="flex flex-col items-center mb-6">
-                <img
-                  className="w-24 h-24 rounded-full border-4 border-gray-300 mb-4"
-                  src={photo}
-                />
-                <h2 className="text-2xl font-semibold text-gray-800">
-                  {userDetail?.name}
-                </h2>
-              </div>
+          <div
+            className="max-w-md p-6 mx-auto bg-white shadow-lg rounded-lg"
+          >
+            <div className="flex flex-col items-center mb-6">
+              <img
+                className="w-24 h-24 rounded-full border-4 border-gray-300 mb-4"
+                src={photo}
+              />
+              <h2 className="text-2xl font-semibold text-gray-800">
+                {userDetail?.name}
+              </h2>
+            </div>
 
-              <div className="space-y-4">
-                <div className="flex justify-between">
-                  <span className="font-medium text-gray-700">Name:</span>
-                  <span className="text-gray-600">{userDetail?.name}</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="font-medium text-gray-700">Class:</span>
-                  <span className="text-gray-600">{userDetail?.className}</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="font-medium text-gray-700">Roll No:</span>
-                  <span className="text-gray-600">{userDetail?.rollNo}</span>
-                </div>
-                <div className="flex justify-between">
-                  <span className="font-medium text-gray-700">Address:</span>
-                  <span className="text-gray-600">{userDetail?.email}</span>
-                </div>
+            <div className="space-y-4">
+              <div className="flex justify-between">
+                <span className="font-medium text-gray-700">Name:</span>
+                <span className="text-gray-600">{userDetail?.name}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-gray-700">Class:</span>
+                <span className="text-gray-600">{userDetail?.className}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-gray-700">Roll No:</span>
+                <span className="text-gray-600">{userDetail?.rollNo}</span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-gray-700">Address:</span>
+                <span className="text-gray-600">{userDetail?.email}</span>
               </div>
             </div>
+          </div>
         </div>
       </div>
 
-        <div className="my-5">
-          <h1 className="text-center text-xl">Your Outstanding</h1>
-        </div>
+      <div className="my-5">
+        <h1 className="text-center text-xl">Your Outstanding</h1>
+      </div>
       <div className="pt-0 pb-10 flex px-0 justify-center md:justify-normal md:px-20">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {paymentDetail?.payment?.map((info, key) => (
@@ -114,7 +111,7 @@ const StudentProfile = () => {
               <h3 className="text-lg text-gray-600">{info.duePayment} BDT</h3>
               <h3>Status : {info.status}</h3>
               {
-                info.status === "Pending" && <button className="bg-green-500 text-white px-3" onClick={()=>{handlePayment(info.month, info.year, info.duePayment)}}>Pay now</button>
+                info.status === "Pending" && <button className="bg-green-500 text-white px-3" onClick={() => { handlePayment(info.month, info.year, info.duePayment) }}>Pay now</button>
               }
             </div>
           ))}
--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,38 +1,12 @@
-
-import { useEffect } from "react";
-import SUCCESSIMAGE from "../../assets/success.gif"
-import { useLocation } from "react-router-dom";
-import useAxiosPublic from "../../Hooks/useAxiosPublic";
-
-
+import SUCCESSIMAGE from "../../assets/success.gif";
 const Success = () => {
-  const axios = useAxiosPublic();
-  const location = useLocation();
-
-  useEffect(() => {
-    const fetchSession = async () => {
-      const sessionId = new URLSearchParams(location.search).get('session_id');
-
-      if (sessionId) {
-        const response = await axios(`http://localhost:5000/api/retrieve/${sessionId}`);
-        console.log(response.data);
-      }
-    };
-
-    fetchSession();
-  }, [location]);
-
+  
   return (
-    <div className='bg-slate-200 w-full max-w-md mx-auto flex justify-center items-center flex-col p-4 m-2 rounded'>
-      <img
-        src={SUCCESSIMAGE}
-        width={150}
-        height={150}
-        alt='success'
-      />
-      <p className='text-green-600 font-bold text-xl'>Payment Successful</p>
+    <div className="bg-slate-200 mt-24 w-full max-w-md mx-auto flex justify-center items-center flex-col p-4 m-2 rounded">
+      <img src={SUCCESSIMAGE} width={150} height={150} alt="success" />
+      <p className="text-green-600 font-bold text-xl">Payment Successful</p>
     </div>
-  )
-}
+  );
+};
 
-export default Success
+export default Success;
